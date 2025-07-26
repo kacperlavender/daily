9c2254748df9ca6ac87bae6097c4c9a3841767a3
Kacper Kiełbasa <kacper.kielbasa5000@gmail.com>
2025-07-27 00:35:48 +0100

    c

---
+++ main.py
@@ -0,0 +1,29 @@
+# Napisz program, który:
+#
+#     Pobierze od użytkownika ciąg liczb całkowitych oddzielonych spacją (np. 4 7 -2 10 3).
+#     Zwróci listę liczb tylko dodatnich i podniesie je do kwadratu.
+#     Jeśli użytkownik poda coś, co nie jest liczbą, program powinien wyświetlić komunikat o błędzie i poprosić o podanie liczb ponownie (czyli aż do skutku).
+
+
+while True:
+    lst = []
+
+    try:
+        user_input = input("podaj liczby oddzielone spacjami: ")
+        numbers = [int(x.strip()) for x in user_input.split(" ")]
+
+        # for i in range(len(numbers)):
+        #     if numbers[i-1] > 0:
+        #         lst.append(numbers[i-1])
+        #     else:
+        #         continue
+
+    except ValueError as e:
+        print(e)
+        print("niepoprawny format. podaj liczby ponownie")
+        continue
+
+    l = [x*x for x in numbers if x > 0]
+    print(l)
+    break
+    ran
