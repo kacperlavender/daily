adcce51a61c575bfebf31c0e173d7987c9f5967f
Kacper Kiełbasa <kacper.kielbasa5000@gmail.com>
2025-07-30 11:20:50 +0100

    c

--- main.py
+++ main.py
@@ -1,24 +1,46 @@
+import string
+
 # Treść zadania:
-#     Napisz funkcję filtruj_i_podnos, która:
-#         Przyjmuje listę liczb całkowitych.
-#         Filtruje z niej liczby parzyste większe niż 10.
-#         Następnie podnosi każdą z tych liczb do potęgi 3 (sześcian).
-#         Zwraca nową listę wynikową.
+#     Napisz funkcję policz_słowa_w_pliku(nazwa_pliku), która:
+#         Otwiera plik tekstowy o nazwie podanej w argumencie.
+#         Liczy wystąpienia wszystkich słów (ignoruj wielkość liter; słowa traktuj jako ciągi rozdzielone spacjami lub znakami interpunkcyjnymi).
+#         Zwraca słownik, gdzie kluczem jest słowo, a wartością liczba jego wystąpień.
+#         W głównym programie zapytaj użytkownika o nazwę pliku (np. tekst.txt).
 
-#     Napisz prosty program, który:
-#         Poprosi użytkownika o podanie liczb całkowitych oddzielonych spacją.
-#         Zamieni je na listę liczb.
-#         Użyje funkcji filtruj_i_podnos do przetworzenia listy.
-#         Wypisze wynik.
+# Wyświetl 5 najczęściej występujących słów w formie:
 
-def filtruj_i_podnos(l: list):
-    # for i in range(len(l)):
-    #     l[i] = int(l[i])
+# słowo1: liczba1
+# słowo2: liczba2
 
-    return [x**3 for x in l if x > 10 and x % 2 == 0]
+file = input("podaj nazwe pliku ktory chcesz sprawdzic: \n")
+dict = {}
 
-text = input("podaj liczby oddzielone spacjami:\n")
-# l = text.split(" ")
-l = [int(x) for x in text.split()]
+try:
+    with open(file) as f:
+        dane = f.read()
+        dane = dane.lower()
+        # czyszczenie danych ze znakow intepunkcyjnych
+        clean_dane = ''.join(ch for ch in dane if ch not in string.punctuation)
 
-print(filtruj_i_podnos(l))
+        word = clean_dane.split("\n")
+
+        for w in word:
+            if w.isalpha():
+                if w in dict:
+                    dict[w] += 1
+                else:
+                    dict[w] = 1
+
+    print(dict)
+
+    # alfabetyczne sortowanie slownika
+    print({k: dict[k] for k in sorted(dict)})
+
+    # 5 najczęstszych słów:
+    print("5 najczęściej występujących słów:")
+    najczestsze = sorted(dict.items(), key=lambda x: x[1], reverse=True)[:5]
+    for slowo, liczba in najczestsze:
+        print(f"{slowo}: {liczba}")
+
+except FileNotFoundError:
+    print("Nie znaleziono pliku!")
